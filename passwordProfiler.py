ba3e5ff3d6334994d757519ab14729933d355c39
Rushil Choksi <55545647+rushilchoksi@users.noreply.github.com>
2022-10-28 20:52:03 +0530

    Developed password profiler
    Takes user's name and date of birth as input and generated 4K to 6K passwords based on testing

---
+++ passwordProfiler.py
@@ -0,0 +1,65 @@
+import string
+from datetime import datetime
+from itertools import combinations
+from random import randint, choice
+
+def returnLeetWords(inputWord):
+    leetCharsList, leetWordsList = [], [inputWord]
+    for charValue in inputWord:
+        if charValue.lower() in mappingDict.keys():
+            leetCharsList.append(charValue)
+
+    for tempWord in leetCharsList:
+        leetWordsList.append(inputWord.replace(tempWord, mappingDict.get(tempWord.lower())))
+
+    return list(set(leetWordsList))
+
+def genPasswordSet(inputWordsList, dobSuffixData):
+    profiledData = []
+    for tempName in inputWordsList:
+        for specialChar in specialCharsList:
+            for dobSuffixValue in list(dobSuffixData):
+                profiledData.append(f'{tempName}{specialChar}{dobSuffixValue}')
+
+        for dateData in parsedDateList:
+            profiledData.append(f'{tempName}{dateData}')
+            profiledData.append(f'{tempName}{dateData}'.lower())
+            profiledData.append(f'{tempName}{dateData}'.upper())
+            profiledData.append(f'{tempName}{dateData}'.swapcase())
+
+        for _ in range(randint(1, 20)):
+            tempRandPasswordDataOne = f'{tempName}'
+            for tempNum in range(randint(1, 5)):
+                tempRandPasswordDataOne += choice(specialCharsList)
+            profiledData.append(tempRandPasswordDataOne)
+
+        for _ in range(randint(1, 20)):
+            tempRandPasswordDataTwo = f'{tempName}'
+            for tempNum in range(randint(1, 5)):
+                tempRandPasswordDataTwo += choice(list(string.digits))
+            profiledData.append(tempRandPasswordDataTwo)
+
+    return list(set(profiledData))
+
+mappingDict = {'a': '@', 'e': '3', 'f': 'ƒ', 'i': '!', 'o': '0', 's': '$', 'y': '¥'}
+specialCharsList = ['!', '@', '#', '$','%', '^', '&', '*', '(', ')']
+
+firstName = input('Enter first name: ')
+lastName = input('Enter last name: ')
+dateOfBirth = input('Enter date of birth (DDMMYYYY): ')
+parsedDate = datetime.strptime(dateOfBirth, '%d%m%Y')
+parsedDateList = [str(parsedDate.day).zfill(2), str(parsedDate.month).zfill(2), parsedDate.year]
+finalInputWordList, lastNameWithLeet = returnLeetWords(firstName), returnLeetWords(lastName)
+finalInputWordList.extend(lastNameWithLeet)
+
+dobSuffixData = set()
+for dobSuffixLength in range(1, 5):
+    for tempSuffixData in combinations(dateOfBirth, dobSuffixLength):
+        dobSuffixData.add(''.join(tempSuffixData))
+
+profiledData = genPasswordSet(finalInputWordList, dobSuffixData)
+profiledData.append(f'{firstName[0]}{lastName[0]}{dateOfBirth}')
+profiledData.append(f'{firstName[0]}{lastName[0]}{dateOfBirth}'.lower())
+
+print(sorted(profiledData))
+print(f'Generated {len(profiledData)} passwords for {firstName} {lastName}.')
