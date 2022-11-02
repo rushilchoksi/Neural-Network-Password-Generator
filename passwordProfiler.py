fcc2ede45be2788e9d1c4ecb755511ac80a34c1b
Dev Patel <56962517+TheCyberAlchemist@users.noreply.github.com>
2022-11-02 11:03:29 +0530

    Created another generate leet words function using another technique

--- passwordProfiler.py
+++ passwordProfiler.py
@@ -1,14 +1,41 @@
 import string
 from datetime import datetime
 from itertools import combinations
-from random import randint, choice
+from random import randint, choice, random
+
+def return_leet_words(inputWord, mappingDict, specialCharsList, outputLength):
+    """
+        uses weighted random technique to return a leet version of the inputWord
+        will replace random characters with leet characters
+    """
+    leet_words_list = []
+    for _ in range(outputLength):
+        changable_chars_indices = []
+        for char_index, char_value in enumerate(inputWord):
+            if char_value.lower() in mappingDict.keys():
+                changable_chars_indices.append(char_index)
+        
+        probaility_of_leet = 0.125
+        offset_to_add = 0.35
+        leet_word = inputWord
+        for _ in changable_chars_indices:
+            rand = random()
+            if rand > probaility_of_leet:
+                random_index = choice(changable_chars_indices)
+                changable_chars_indices.remove(random_index)
+                leet_word = leet_word[:random_index] + mappingDict[leet_word[random_index].lower()] + leet_word[random_index + 1:]
+                probaility_of_leet += offset_to_add
+            
+        # print(leet_word)
+        leet_words_list.append(leet_word)
+    return leet_words_list
+
 
 def returnLeetWords(inputWord, mappingDict, specialCharsList):
     leetCharsList, leetWordsList = [], [inputWord]
     for charValue in inputWord:
         if charValue.lower() in mappingDict.keys():
             leetCharsList.append(charValue)
-
     for tempWord in leetCharsList:
         leetWordsList.append(inputWord.replace(tempWord, mappingDict.get(tempWord.lower())))
 
@@ -65,3 +92,11 @@ def generateWordList(firstName, lastName, dateOfBirth):
             wordlistFile.write(f'{tempPassword}\n')
 
     return outputFileName
+
+if __name__ == '__main__':
+    firstName, lastName, dateOfBirth = 'Dev', 'Patel', '06092001'
+    mappingDict = {'a': '@', 'e': '3', 'f': 'ƒ', 'i': '!', 'o': '0', 's': '$', 'y': '¥'}
+    specialCharsList = ['!', '@', '#', '$','%', '^', '&', '*', '(', ')']
+    keyword="DevPatelisawsome"
+    # print(generateWordList(firstName, lastName, dateOfBirth))
+    print(return_leet_words(keyword, mappingDict, specialCharsList,4))
