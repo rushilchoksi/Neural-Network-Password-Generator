0de69528a3e7f0c44a9fdf0248638899e9c69419
Rushil Choksi <55545647+rushilchoksi@users.noreply.github.com>
2022-11-05 18:35:13 +0530

    Modified API to serve RNN

--- breachAPI.py
+++ breachAPI.py
@@ -3,17 +3,22 @@ import hashlib
 import requests
 from getpass import getpass
 
-breachPasswordFlag, userPassword = False, getpass('Enter password: ')
-hashVal = hashlib.sha1(userPassword.encode()).hexdigest().upper()
+def checkPassword(hashVal, breachPasswordFlag):
+    requestData = requests.get(f'https://api.pwnedpasswords.com/range/{hashVal[:5]}')
+    for tempPasswordHash in requestData.text.split('\r\n'):
+        tempPasswordVal = tempPasswordHash.split(":")
+        if hashVal == f'{hashVal[:5]}{tempPasswordVal[0]}':
+            breachPasswordFlag = True
+            print(f'\nThe password provided as input has been compromised in {tempPasswordVal[1]} data breaches.')
 
-requestData = requests.get(f'https://api.pwnedpasswords.com/range/{hashVal[:5]}')
-for tempPasswordHash in requestData.text.split('\r\n'):
-    tempPasswordVal = tempPasswordHash.split(":")
-    if hashVal == f'{hashVal[:5]}{tempPasswordVal[0]}':
-        breachPasswordFlag = True
-        print(f'\nThe password provided as input has been compromised in {tempPasswordVal[1]} data breaches.')
+    return breachPasswordFlag
 
-if breachPasswordFlag == False:
-    print('\nYour password is secure as of now and now detected in any of the data breaches! 🤙🏻')
-else:
-    print('Your password has been compromised in one or more data breaches, use our software to stay safe! 😂')
+if __name__ == "__main__":
+    breachPasswordFlag, userPassword = False, getpass('Enter password: ')
+    hashVal = hashlib.sha1(userPassword.encode()).hexdigest().upper()
+
+    breachPasswordFlag = checkPassword(hashVal, False)
+    if breachPasswordFlag == False:
+        print('\nYour password is secure as of now and now detected in any of the data breaches! 🤙🏻')
+    else:
+        print('Your password has been compromised in one or more data breaches, use our software to stay safe! 😂')
